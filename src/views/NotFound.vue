46c3f578b0d874938f6de60da976131eb203819e
Yayanjay <yayanabdillah01@gmail.com>
2021-11-21 06:47:24 +0700

    add error page handling 404 and 401

---
+++ src/views/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="mt-10">
+
+		<v-card >
+			<v-card-action>
+				<v-btn class="ma-2" to="/">
+					<v-icon>fa-arrow-left</v-icon>
+					Go back
+				</v-btn>
+			</v-card-action>
+			<v-img 
+				class="mx-auto"
+				width="70vh" 
+				src="https://user-images.githubusercontent.com/34501764/142715690-5f023f85-681a-44d2-a623-3e1d1795e046.jpg">
+			</v-img>
+		</v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Unauthorized.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container class="mt-10">
+
+    <v-card >
+        <v-card-action>
+            <v-btn class="ma-2" to="/">
+                <v-icon>fa-arrow-left</v-icon>
+                Go back
+            </v-btn>
+        </v-card-action>
+        <v-img 
+            class="mx-auto"
+            width="70vh" 
+            src="https://user-images.githubusercontent.com/34501764/142623186-bfcdfa1a-2502-4212-a516-61472cefdf2a.jpg">
+
+        </v-img>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "unauthorized"
+}
+</script>
+
+<style>
+.tes {
+    border: 4px solid black;
+}
+</style>
